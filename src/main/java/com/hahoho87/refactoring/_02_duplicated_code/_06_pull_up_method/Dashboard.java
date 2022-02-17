c99f5a0442ffdf2ae18e72efdd19b79a5399d0cb
bky4564 <bky4564@hunet.co.kr>
2022-02-17 12:41:12 +0900

    pull up method

--- src/main/java/com/hahoho87/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/com/hahoho87/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,6 +1,12 @@
 package com.hahoho87.refactoring._02_duplicated_code._06_pull_up_method;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Dashboard {
 
@@ -10,5 +16,19 @@ public class Dashboard {
 
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
         participantDashboard.printParticipants(15);
+    }
+
+    protected void printUserNames(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get reviewers
+        Set<String> reviewers = new HashSet<>();
+        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+
+        // Print reviewers
+        reviewers.forEach(System.out::println);
     }
 }
