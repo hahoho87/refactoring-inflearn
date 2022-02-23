0009d49d0595d541117b6a04420f2d8fecc0572f
bky4564 <bky4564@hunet.co.kr>
2022-02-23 13:04:53 +0900

    세터 제거

--- src/test/java/com/hahoho87/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
+++ src/test/java/com/hahoho87/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
@@ -2,14 +2,13 @@ package com.hahoho87.refactoring._06_mutable_data._20_remove_setting_method;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
 
     @Test
     void person() {
-        Person person = new Person();
-        person.setId(10);
+        Person person = new Person(10);
         person.setName("keesun");
         assertEquals(10, person.getId());
         assertEquals("keesun", person.getName());
