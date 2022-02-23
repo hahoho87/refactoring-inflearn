22597a98e168105243994160f45772108f1ac710
bky4564 <bky4564@hunet.co.kr>
2022-02-23 13:01:53 +0900

    질의함수와 변경 함수 분리

--- src/test/java/com/hahoho87/refactoring/_06_mutable_data/_19_separate_query_from_modifier/BillingTest.java
+++ src/test/java/com/hahoho87/refactoring/_06_mutable_data/_19_separate_query_from_modifier/BillingTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BillingTest {
 
@@ -12,7 +12,7 @@ class BillingTest {
     void totalOutstanding() {
         Billing billing = new Billing(new Customer("keesun", List.of(new Invoice(20), new Invoice(30))),
                 new EmailGateway());
-        assertEquals(50d, billing.getTotalOutstandingAndSendBill());
+        assertEquals(50d, billing.getTotalOutstanding());
     }
 
 }
