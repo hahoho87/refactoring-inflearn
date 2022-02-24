bf38d48c99f8bfc9a6dcee9b0cb0bae194752cba
bky4564 <bky4564@hunet.co.kr>
2022-02-24 12:15:56 +0900

    파생 변수를 질의 함수로 변경

--- src/main/java/com/hahoho87/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
+++ src/main/java/com/hahoho87/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductionPlan {
-
-    private double production;
     private List<Double> adjustments = new ArrayList<>();
 
     public void applyAdjustment(double adjustment) {
         this.adjustments.add(adjustment);
-        this.production += adjustment;
     }
 
     public double getProduction() {
-        return this.production;
+        return calculatedProduction();
+    }
+
+    private double calculatedProduction() {
+        return this.adjustments.stream().reduce((double) 0, Double::sum);
     }
 }
