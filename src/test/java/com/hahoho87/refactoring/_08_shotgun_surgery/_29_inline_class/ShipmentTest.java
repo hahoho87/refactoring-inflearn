98d9fcdf942e503191a5b009674870400092e39d
bky4564 <bky4564@hunet.co.kr>
2022-02-25 12:28:26 +0900

    클래스 인라인

--- src/test/java/com/hahoho87/refactoring/_08_shotgun_surgery/_29_inline_class/ShipmentTest.java
+++ src/test/java/com/hahoho87/refactoring/_08_shotgun_surgery/_29_inline_class/ShipmentTest.java
@@ -2,13 +2,13 @@ package com.hahoho87.refactoring._08_shotgun_surgery._29_inline_class;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShipmentTest {
 
     @Test
     void trackingInfo() {
-        Shipment shipment = new Shipment(new TrackingInformation("UPS", "12345"));
+        Shipment shipment = new Shipment("UPS", "12345");
         assertEquals("UPS: 12345", shipment.getTrackingInfo());
     }
 
