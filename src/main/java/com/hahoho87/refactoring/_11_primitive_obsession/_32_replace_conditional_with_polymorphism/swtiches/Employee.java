73b2aafb6f0328153d73d0299eb411a22d464921
bky4564 <bky4564@hunet.co.kr>
2022-02-28 13:22:08 +0900

    조건부 로직을 다형성으로 변경

--- src/main/java/com/hahoho87/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
+++ src/main/java/com/hahoho87/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
@@ -4,29 +4,20 @@ import java.util.List;
 
 public class Employee {
 
-    private String type;
+    protected List<String> availableProjects;
 
-    private List<String> availableProjects;
-
-    public Employee(String type, List<String> availableProjects) {
-        this.type = type;
+    public Employee(List<String> availableProjects) {
         this.availableProjects = availableProjects;
     }
 
+    public Employee() {
+    }
+
     public int vacationHours() {
-        return switch (type) {
-            case "full-time" -> 120;
-            case "part-time" -> 80;
-            case "temporal" -> 32;
-            default -> 0;
-        };
+        return 0;
     }
 
     public boolean canAccessTo(String project) {
-        return switch (type) {
-            case "full-time" -> true;
-            case "part-time", "temporal" -> this.availableProjects.contains(project);
-            default -> false;
-        };
+        return false;
     }
 }
--- src/test/java/com/hahoho87/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRatingTest.java
+++ src/test/java/com/hahoho87/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRatingTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VoyageRatingTest {
 
     @Test
     void westIndies() {
-        VoyageRating voyageRating = new VoyageRating(new Voyage("west-inides", 10),
+        VoyageRating voyageRating = RatingFactory.createRating(new Voyage("west-inides", 10),
                 List.of(new VoyageHistory("east-indies", 5),
                         new VoyageHistory("west-indies", 15),
                         new VoyageHistory("china", -2),
@@ -20,7 +20,7 @@ class VoyageRatingTest {
 
     @Test
     void china() {
-        VoyageRating voyageRating = new VoyageRating(new Voyage("china", 10),
+        VoyageRating voyageRating = RatingFactory.createRating(new Voyage("china", 10),
                 List.of(new VoyageHistory("east-indies", 5),
                         new VoyageHistory("west-indies", 15),
                         new VoyageHistory("china", -2),
