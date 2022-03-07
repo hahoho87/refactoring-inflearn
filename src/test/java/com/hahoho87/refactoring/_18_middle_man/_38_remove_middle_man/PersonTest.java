93f80bea1a2699a266786cff67dc52248eb360a3
bky4564 <bky4564@hunet.co.kr>
2022-03-08 08:22:13 +0900

    중재자 제거

--- src/test/java/com/hahoho87/refactoring/_18_middle_man/_38_remove_middle_man/PersonTest.java
+++ src/test/java/com/hahoho87/refactoring/_18_middle_man/_38_remove_middle_man/PersonTest.java
@@ -2,7 +2,7 @@ package com.hahoho87.refactoring._18_middle_man._38_remove_middle_man;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
 
@@ -10,7 +10,7 @@ class PersonTest {
     void getManager() {
         Person nick = new Person("nick", null);
         Person keesun = new Person("keesun", new Department(nick));
-        assertEquals(nick, keesun.getManager());
+        assertEquals(nick, keesun.getDepartment().getManager());
     }
 
 }
