f141162748db03cc73c5db4b7db05983148decb0
bky4564 <bky4564@hunet.co.kr>
2022-03-08 12:16:00 +0900

    내부자 거래

--- src/test/java/com/hahoho87/refactoring/_19_insider_trading/CheckInTest.java
+++ src/test/java/com/hahoho87/refactoring/_19_insider_trading/CheckInTest.java
@@ -4,16 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CheckInTest {
 
     @Test
     void isFastPass() {
         CheckIn checkIn = new CheckIn();
-        assertTrue(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), true)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), false)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2022, 1, 2), true)));
+        assertTrue(new Ticket(LocalDate.of(2021, 12, 31), true).isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2021, 12, 31), false).isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2022, 1, 2), true).isFastPass());
     }
 
 }
