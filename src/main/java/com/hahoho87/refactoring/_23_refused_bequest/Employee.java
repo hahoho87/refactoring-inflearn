6a0eca6c314b22236d2541876ed5778ad7e231c1
bky4564 <bky4564@hunet.co.kr>
2022-03-10 08:40:21 +0900

    상속포기

--- src/main/java/com/hahoho87/refactoring/_23_refused_bequest/Employee.java
+++ src/main/java/com/hahoho87/refactoring/_23_refused_bequest/Employee.java
@@ -2,10 +2,4 @@ package com.hahoho87.refactoring._23_refused_bequest;
 
 public class Employee {
 
-    protected Quota quota;
-
-    protected Quota getQuota() {
-        return new Quota();
-    }
-
 }
--- src/main/java/com/hahoho87/refactoring/_23_refused_bequest/Salesman.java
+++ src/main/java/com/hahoho87/refactoring/_23_refused_bequest/Salesman.java
@@ -2,5 +2,10 @@ package com.hahoho87.refactoring._23_refused_bequest;
 
 public class Salesman extends Employee {
 
+    protected Quota quota;
+
+    protected Quota getQuota() {
+        return new Quota();
+    }
 }
 
